c8d4c6a785a381550113142ca04717056b4d06f8
Rikhil2 <91702512+Rikhil2@users.noreply.github.com>
2022-03-10 22:14:08 -0500

    Create Viète.js

---
+++ Viète.js
@@ -0,0 +1,9 @@
+let py = 1;
+for (let i=0; i<10; i++) {
+  let factor1 = 2**0.5;
+  let factor2 = (2+factor1)**0.5;
+  py *= (factor2/2);
+  factor1 = factor2;
+}
+//the loop yields 2/pi;
+createDiv((1/py)/2);
